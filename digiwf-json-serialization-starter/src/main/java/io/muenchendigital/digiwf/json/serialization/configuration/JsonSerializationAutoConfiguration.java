e19dd6f497953680942c45b0c82eebd9a175dedf
Lukas Mösle <ext.dl.moesle@muenchen.de>
2022-02-23 15:39:47 +0100

    feat: extract json schema serializationService into spring boot starter

--- digiwf-json-serialization-starter/src/main/java/io/muenchendigital/digiwf/json/serialization/configuration/JsonSerializationAutoConfiguration.java
+++ digiwf-json-serialization-starter/src/main/java/io/muenchendigital/digiwf/json/serialization/configuration/JsonSerializationAutoConfiguration.java
@@ -1,7 +1,6 @@
 package io.muenchendigital.digiwf.json.serialization.configuration;
 
 import io.muenchendigital.digiwf.json.serialization.SerializationService;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 public class JsonSerializationAutoConfiguration {
 
     @Bean
-    @ConditionalOnBean
     public SerializationService serializationService() {
         return new SerializationService();
     }
