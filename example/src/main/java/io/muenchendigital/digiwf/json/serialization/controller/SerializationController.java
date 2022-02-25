a9594891f7f0aa847d4417699e0b001b2ad89203
lmoesle <lukas95m@yahoo.de>
2022-02-25 12:42:26 +0100

    feat: added example app for json schema serialization starter

---
+++ example/src/main/java/io/muenchendigital/digiwf/json/serialization/controller/SerializationController.java
@@ -0,0 +1,47 @@
+package io.muenchendigital.digiwf.json.serialization.controller;
+
+import io.muenchendigital.digiwf.json.serialization.JsonSchemaSerializationService;
+import io.muenchendigital.digiwf.json.serialization.dto.DataDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@RestController
+public class SerializationController {
+
+    // inject JsonSchemaSerializationService
+    private final JsonSchemaSerializationService jsonSchemaSerializationService;
+
+    @PostMapping(path = "/serialize")
+    public Map<String, Object> serialize(@RequestBody final DataDto body) throws IOException, URISyntaxException {
+        final String rawSchema = this.getSchemaString(body.getSchema());
+        return jsonSchemaSerializationService.serializeData(rawSchema, body.getData(), body.getPreviousData());
+    }
+
+    @PostMapping(path = "/deserialize")
+    public Map<String, Object> deserialize(@RequestBody final DataDto body) throws IOException, URISyntaxException {
+        final String rawSchema = this.getSchemaString(body.getSchema());
+        return jsonSchemaSerializationService.deserializeData(rawSchema, body.getData());
+    }
+
+    /**
+     * Helper Function
+     * Loads the json schema from resources folder
+     *
+     * @param path
+     * @return
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    private String getSchemaString(final String path) throws IOException, URISyntaxException {
+        return new String(Files.readAllBytes(Paths.get(this.getClass().getResource(path).toURI())));
+    }
+}
