83a8b0af61cf70235ddd416eb41255a64854ecc2
dominikhorn93 <dominik.horn93@gmail.com>
2022-03-23 22:48:21 +0100

    feat: JsonSchemaValidator Bean

--- digiwf-json-serialization-starter/src/main/java/io/muenchendigital/digiwf/json/serialization/configuration/JsonSerializationAutoConfiguration.java
+++ digiwf-json-serialization-starter/src/main/java/io/muenchendigital/digiwf/json/serialization/configuration/JsonSerializationAutoConfiguration.java
@@ -3,6 +3,7 @@ package io.muenchendigital.digiwf.json.serialization.configuration;
 import io.muenchendigital.digiwf.json.serialization.JsonSerializationService;
 import io.muenchendigital.digiwf.json.serialization.serializer.JsonSerializer;
 import io.muenchendigital.digiwf.json.serialization.serializer.JsonSerializerImpl;
+import io.muenchendigital.digiwf.json.validation.JsonSchemaValidator;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +21,11 @@ public class JsonSerializationAutoConfiguration {
     @Bean
     public JsonSerializationService jsonSchemaSerializationService(final JsonSerializer serializer) {
         return new JsonSerializationService(serializer);
+    }
+
+    @Bean
+    public JsonSchemaValidator jsonSchemaValidator() {
+        return new JsonSchemaValidator();
     }
 
 }
